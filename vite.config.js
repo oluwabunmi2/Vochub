f9c45f9ad734666b6af7e8e9724dae4dfe51722e
oluwabunmi2 <106445167+oluwabunmi2@users.noreply.github.com>
2024-11-25 02:18:42 +0100

    checking for errors

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',    // Allow external access (important for Render)
-    port: process.env.PORT || 10000,  // Use Render's provided PORT or fallback to 10000
-  },
+  plugins: [react()],
 })
